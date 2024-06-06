9cffd9c8a273bb858631b97fcd2853de1aee0734
Pablo Sánchez <sancheztorrespablo@gmail.com>
2024-06-06 21:08:13 +0200

    Clase Jueves. Creamos activity InsertarClientesActivity para insertar en FireBase. Creamos un botón para listar por el log la DataBase. Creamos la función de listado en la propia clase insertar (pendiente de realizar Rycycler)

--- app/src/main/java/gal/cntg/cntgapp/realtimedatabase/InsertarClientesActivity.kt
+++ app/src/main/java/gal/cntg/cntgapp/realtimedatabase/InsertarClientesActivity.kt
@@ -36,18 +36,66 @@ class InsertarClientesActivity : AppCompatActivity() {
             binding.editTextLocalidad.text.toString(), binding.editTextNombreCliente.text.toString(),
             binding.editTextEmail.text.toString(), binding.editTextNacionalidad.text.toString())
                     // la clave es opcional, se la daremos más adelante pq no la sabemos.
-        val idClaveCliente = this.databaseReference.push().key // generamos un id desde la BBDD.
 
+        val idClaveCliente = this.databaseReference.push().key // generamos un id desde la BBDD.
         cliente.clave = idClaveCliente!!
 
         // así insertamos el cliente, con la ruta de la BBDD no relacional. Clientes/idclaveCliente/Cliente
         // le indicamos que el id no será null con !!
         this.databaseReference.child("Clientes").child(idClaveCliente).setValue(cliente).addOnCompleteListener {
             tarea -> Toast.makeText(this, "CLIENTE INSERTADO EN BBDD", Toast.LENGTH_LONG).show()
-        }.addOnFailureListener {
+            finish()
+            startActivity(this.intent)
+        }.addOnFailureListener { // Controlamos si falla el insertado.
             Toast.makeText(this, "ERROR!! CLIENTE NO INSERTADO!!", Toast.LENGTH_LONG).show()
             Log.e("CNTG_APP", "ERROR INSERTANDO CLIENTE", it) // it sería el mensaje de la excepción.
         }
+    }
+
+    fun listarClientes(view: View) {
+
+        Log.d("CNTG_APP", "Mostrando clientes de la BD")
+        this.databaseReference.child("Clientes").get().addOnSuccessListener {
+                datos ->
+            var claveDocumento = datos.key
+            Log.d("CNTG_APP", "Clave documento = $claveDocumento")
+            var lista = datos.value as Map<String, Map<String, Any>> //los registros
+            var entradas = lista.entries
+            var nclis = entradas.size
+            var listaClientes = ArrayList<Cliente>()
+
+            entradas.forEach { (claveId, valor) ->
+                Log.d("CNTG_APP", "id cliente = $claveId")
+                Log.d("CNTG_APP", "nombre = ${valor.get("nombre")}")
+                Log.d("CNTG_APP", "email = ${valor.get("email")}")
+                Log.d("CNTG_APP", "localidad = ${valor.get("localidad")}")
+                Log.d("CNTG_APP", "edad = ${valor.get("edad")}")
+                Log.d("CNTG_APP", "nacionalidad = ${valor.get("nacionalidad")}")
+                //(val edad:Int, val localidad:String, val nombre: String, val email:String, val nacionalidad:String)
+                var cliente = Cliente (valor.get("edad") as Long, valor.get("localidad").toString(),
+                    valor.get("nombre").toString(), valor.get("email").toString(), valor.get("nacionalidad").toString(), claveId)
+
+                listaClientes.add(cliente)
+                if (listaClientes.size==nclis)
+                {
+                    mostrarClientes(listaClientes)
+                }
+
+            }
+        }
+    }
+
+    private fun mostrarClientes(listaClientes: ArrayList<Cliente>) {
+
+        listaClientes.forEach{Log.d("CNTG_APP", "${it}")}
+
+        //TODO lo suyo, sería hacer un RECYCLER
+        //borrar registro por clave
+        /**
+         *  var registro = bdref.child("/clientes").child(id)
+         *   registro.removeValue().addOnCompleteListener { tarea -> }
+         */
+
     }
 
 
